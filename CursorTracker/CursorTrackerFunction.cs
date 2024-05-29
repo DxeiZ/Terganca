8f4c7e5a6b9b6c4f7f640b85425a2747b81aab68
DxeiZ <79997967+DxeiZ@users.noreply.github.com>
2024-05-29 16:35:08 +0400

    Add files via upload

---
+++ CursorTracker/CursorTrackerFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+public class CursorTrackerFunction
+{
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out POINT lpPoint);
+
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    public static POINT GetCursorPosition()
+    {
+        GetCursorPos(out POINT lpPoint);
+        return lpPoint;
+    }
+}
---
+++ CursorTracker/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CursorTracker
+{
+    public partial class Main : Form
+    {
+        private Label lblCoordinates;
+        private Timer timer;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            lblCoordinates = new Label();
+            timer = new Timer();
+
+            SuspendLayout();
+
+            // 
+            // lblCoordinates
+            // 
+            lblCoordinates.AutoSize = true;
+            lblCoordinates.Location = new System.Drawing.Point(12, 9);
+            lblCoordinates.Name = "lblCoordinates";
+            lblCoordinates.Size = new System.Drawing.Size(100, 23);
+            lblCoordinates.TabIndex = 0;
+            lblCoordinates.Text = "X: 0, Y: 0";
+
+            // 
+            // timer
+            // 
+            timer.Interval = 100; // 100 ms
+            timer.Tick += new EventHandler(Timer_Tick);
+
+            // 
+            // Main
+            // 
+            ClientSize = new System.Drawing.Size(284, 41);
+            Controls.Add(lblCoordinates);
+            Name = "Main";
+            Text = "Fare Koordinat İzleyici";
+            Load += new EventHandler(MainForm_Load);
+            StartPosition = FormStartPosition.CenterScreen;
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var position = CursorTrackerFunction.GetCursorPosition();
+            lblCoordinates.Text = $"X: {position.X}, Y: {position.Y}";
+        }
+    }
+}
