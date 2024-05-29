2e98f091a9f5a49ce93c343c05aabe1b298c677c
DxeiZ <79997967+DxeiZ@users.noreply.github.com>
2024-05-29 18:35:54 +0400

    Add files via upload

---
+++ Automation/AutomationFunction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+public class AutomationFunction
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+    public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+    public const int MOUSEEVENTF_LEFTUP = 0x04;
+    public const int MOUSEEVENTF_MOVE = 0x0001;
+
+    public const int KEYEVENTF_KEYDOWN = 0x0000;
+    public const int KEYEVENTF_KEYUP = 0x0002;
+
+    public static void SimulateMouseClick(int x, int y)
+    {
+        SetCursorPosition(x, y);
+        mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+    }
+
+    public static void SetCursorPosition(int x, int y)
+    {
+        Cursor.Position = new System.Drawing.Point(x, y);
+    }
+
+    public static void SimulateKeyPress(byte keyCode)
+    {
+        keybd_event(keyCode, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+        Thread.Sleep(100);
+        keybd_event(keyCode, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+    }
+}
