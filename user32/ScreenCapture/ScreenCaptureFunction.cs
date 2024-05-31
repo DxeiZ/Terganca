936c937b039642758594f72774294d37997e5d0a
DxeiZ <79997967+DxeiZ@users.noreply.github.com>
2024-06-01 03:54:02 +0400

    Add files via upload

---
+++ user32/ScreenCapture/ScreenCaptureFunction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class ScreenCaptureFunction
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetDC(IntPtr hwnd);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+    [DllImport("gdi32.dll")]
+    private static extern IntPtr DeleteObject(IntPtr hObject);
+
+    [DllImport("gdi32.dll")]
+    private static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight,
+        IntPtr hdcSrc, int nXSrc, int nYSrc, int dwRop);
+
+    private const int SRCCOPY = 0x00CC0020;
+
+    public static Bitmap CaptureScreen()
+    {
+        Rectangle bounds = Screen.PrimaryScreen.Bounds;
+        Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+
+        using (Graphics graphics = Graphics.FromImage(bitmap))
+        {
+            IntPtr hdcDest = graphics.GetHdc();
+            IntPtr hdcSrc = GetDC(IntPtr.Zero);
+            BitBlt(hdcDest, 0, 0, bounds.Width, bounds.Height, hdcSrc, 0, 0, SRCCOPY);
+            graphics.ReleaseHdc(hdcDest);
+            ReleaseDC(IntPtr.Zero, hdcSrc);
+        }
+
+        return bitmap;
+    }
+}
---
+++ user32/WindowManager/WindowManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terganca
+{
+    public class WindowManager
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        private const int SW_MINIMIZE = 6;
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_RESTORE = 9;
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        public static List<WindowInfo> GetOpenWindows()
+        {
+            var windows = new List<WindowInfo>();
+
+            EnumWindows((hWnd, lParam) =>
+            {
+                if (IsWindowVisible(hWnd))
+                {
+                    var length = GetWindowTextLength(hWnd);
+                    if (length > 0)
+                    {
+                        var builder = new StringBuilder(length);
+                        GetWindowText(hWnd, builder, length + 1);
+                        windows.Add(new WindowInfo { Handle = hWnd, Title = builder.ToString() });
+                    }
+                }
+                return true;
+            }, IntPtr.Zero);
+
+            return windows;
+        }
+
+        public static void MinimizeWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_MINIMIZE);
+        }
+
+        public static void MaximizeWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_MAXIMIZE);
+        }
+
+        public static void RestoreWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_RESTORE);
+        }
+
+        public static void BringToFront(IntPtr hWnd)
+        {
+            SetForegroundWindow(hWnd);
+        }
+
+        private static int GetWindowTextLength(IntPtr hWnd)
+        {
+            return SendMessage(hWnd, WM_GETTEXTLENGTH, IntPtr.Zero, IntPtr.Zero).ToInt32();
+        }
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private const uint WM_GETTEXTLENGTH = 0x000E;
+
+        public class WindowInfo
+        {
+            public IntPtr Handle { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
